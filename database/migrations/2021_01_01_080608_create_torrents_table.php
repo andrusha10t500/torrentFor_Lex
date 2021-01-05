af84be98bb448784ec5588396a8e689d9a43c5d0
andrusha10t500 <andrusha10t500@gmail.com>
2021-01-06 01:01:48 +1000

    добавил миграцию для torrents

---
+++ database/migrations/2021_01_01_080608_create_torrents_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTorrentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('torrents', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->addColumn('string','user',[50])->nullable();
+//            $table->string('user',50)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('torrents');
+    }
+}
