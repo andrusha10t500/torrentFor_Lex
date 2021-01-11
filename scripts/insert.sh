d5e99c21982a0c6b1b8abe70742b505d810c3f97
andrusha10t500 <andrusha10t500@gmail.com>
2021-01-11 21:35:23 +1000

    waiting to toogle component

--- scripts/insert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DBUSER=root
-DBPASSWORD=qt67uy
-DATABASE=torrent
-
-for i in public/torrents/*; do mysql -u$DBUSER -p$DBPASSWORD -D$DATABASE -e "INSERT INTO torrent (\`created_at\`, \`updated_at\`,\`user\`, \`torrent\`, \`download\`, \`when_downloaded\`) VALUES (now(), now(),'admin', '${i##*/}', 1, now())" ; done
